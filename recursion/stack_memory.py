06ad4454857f038308f7f96c2295c15873931f9c
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-16 11:17:22 -0500

    recursion practice

--- recursion/stack_memory.py
+++ recursion/stack_memory.py
@@ -8,10 +8,28 @@ def second_method():
 
 def third_method():
     fourth_method()
-    print("I am the third method")
+    print("I am the third method") # first method popped of the stack
     
 def fourth_method():
-    print("I am the fourth method")
+    print("I am the fourth method") # does not get inserted on the stack memory
 
 first_method() 
 # first see fourth_method run
+# note: stack works using LIFO
+
+def recursive_method(n):
+    if n < 1:
+        print("n is less than 1")
+        
+    else:
+        recursive_method(n -1) # does not have to be last thing in method
+        print(n) # note how can print the value after calling method -- note what happens when move this up a line
+        
+recursive_method(4)
+
+def fibonacci(n):
+    if(n <= 2):
+        return 1
+    return fibonacci(n -1) + fibonacci(n -2)
+
+print(fibonacci(10))
