dafc6547828401da0ac00186f2b366ca9b2fe41e
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-18 15:46:46 -0500

    gcd

---
+++ recursion_questions/decimal_to_binary.py
@@ -0,0 +1,9 @@
+def to_binary(n):
+    pass
+
+
+# how to convert a number from decimal to binary using recursion
+# 1. recursive case
+# divide the number by 2
+# get the integer quotient for the next iteration (result of dividing one number by another)
+# get the remainder for the binary digit
