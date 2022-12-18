53bd7077f055bc793e4e6397cdd24df2a8118f65
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-18 16:49:50 -0500

    decimal to binary

--- recursion_questions/decimal_to_binary.py
+++ recursion_questions/decimal_to_binary.py
@@ -1,9 +1,17 @@
 def to_binary(n):
-    pass
+    assert int(n) == n, "The parameter must be an integer"
+    if n == 0:
+        return 0
+    
+    return n % 2 + 10 * to_binary(int(n / 2))
 
 
 # how to convert a number from decimal to binary using recursion
 # 1. recursive case
 # divide the number by 2
 # get the integer quotient for the next iteration (result of dividing one number by another)
-# get the remainder for the binary digit
+# get the remainder for the binary digit (quotient - result of division)
+
+print(to_binary(10))
+print(to_binary(13))
+print(to_binary(12.5))
