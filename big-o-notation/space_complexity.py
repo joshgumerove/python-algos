769308518175a6a4737e22e8db63fbe1cc4b87de
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-19 20:38:27 -0500

    analyzing complexity

--- big-o-notation/space_complexity.py
+++ big-o-notation/space_complexity.py
@@ -24,3 +24,29 @@ def pair_sum_sequence(n):
 
 def pair_sum(a, b):
     return a + b
+
+def find_biggest_number(sampleArray):
+    biggest_number = sampleArray[0]
+
+    for index in range(1, len(sampleArray)):
+        if biggest_number < sampleArray[index]:
+            biggest_number = sampleArray[index]
+    print(biggest_number)
+    
+find_biggest_number([1, 4, 10, 3, 11, 30, 25])
+
+def findMaxRec(sampleArray, n):
+    if n == 1:
+        return sampleArray[0]
+
+    return max(sampleArray[n - 1], findMaxRec(sampleArray, n -1)) # if one then we would be getting the first element by subtracting 1
+
+print(findMaxRec([11, 4, 12, 7], 4), "findMaxRec")
+
+def combinedName(nameArray, n):
+    if n == 1:
+        return nameArray[0]
+    
+    return combinedName(nameArray, n -1) + nameArray[n -1]
+
+print(combinedName(["j", "o", "s", "h"], 4))
