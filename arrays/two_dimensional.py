25f07b1b1322b60da11612e517366b1b6f469b27
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-04 12:36:50 -0500

    2D array

---
+++ arrays/two_dimensional.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+# creating 2D (lesson 1)
+
+# representing multiple temperatures over multiple days
+
+# Day 1 - 11, 15, 10, 6
+# Day 2 - 10, 14, 11, 5
+# Day 3 - 12, 17, 12, 8
+# Day 4 - 11, 18, 14, 9
+
+twoDArray = np.array([[11, 15, 10, 6],
+                      [10, 14, 11, 5],
+                      [12, 17, 12, 8],
+                      [11, 18, 14, 9]
+                      ]) # better time complexity to instantiate with values right away
+
+print(twoDArray)
+
+# insertion 2D arrays (Lesson 2)
+
+# format must be matrix format to be recognized as 2D array
+
