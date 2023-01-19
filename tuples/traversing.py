a99cfc1fcb6c99ab843ea701540d5ddf55c6e555
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-19 17:33:37 -0500

    searching elements in tuples

--- tuples/traversing.py
+++ tuples/traversing.py
@@ -5,3 +5,15 @@ for i in range(len(tuple1)):
     
 for character in tuple1: 
     print(character)
+    
+# searching for an element in a tuple
+
+print("a" in tuple1) # will return true (note time complexity: O(N))
+
+# index() method:
+print(tuple1.index("c")) # will raise error if not in tuple
+
+if "c" in tuple1:
+    print(tuple1.index("c"))
+
+
