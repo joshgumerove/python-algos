deb87ef6953960ac45ee85e8abefe3e74484e821
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-19 16:39:21 -0500

    accessing elements in tuple

---
+++ tuples/access.py
@@ -0,0 +1,9 @@
+# tuples in memory and accessing elements
+
+# accessing elements in a tuple:
+
+tuple1 = ("a", "b", "c", "d", "e")
+print(tuple1[1]) # access the same way as we do a list
+print(tuple1[1:3]) # ('b', 'c')
+
+# tuple1[1] = "F" this will throw an error since immutable
