02e8cdab0647c31f08f3a0e3f320608fc21a38e4
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-19 17:40:29 -0500

    tuple operations overview

---
+++ tuples/operations.py
@@ -0,0 +1,15 @@
+# tuple operations/functions:
+
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = (1, 2, 6, 9, 8, 7)
+
+print(tuple1 + tuple2) # concatenates two tuples together
+print(tuple1 * 4) # will repeat each element 4 times
+
+print(tuple2.count(8))
+print(tuple2.count(100))
+print(len(tuple2))
+print(tuple2.index(8))
+print(max(tuple2))
+print(min(tuple2))
+print(tuple([1, 2, 3])) # convert list to tuple
