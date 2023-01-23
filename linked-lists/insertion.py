bf0b9c454502add1ba75aa5c59e7dc1e56796f0d
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-23 15:52:37 -0500

    insertion into SLL

--- linked-lists/insertion.py
+++ linked-lists/insertion.py
@@ -13,6 +13,37 @@ class LinkedList():
         while node:
             yield node
             node = node.next
+            
+    def insert_into_list(self, value, location):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            if location == 0:
+                current_head = self.head
+                new_head = new_node
+                new_head.next = current_head
+                self.head = new_head
+            elif location == 1: # inserting at the end of the list (do not like the lecture functionality)
+                current_tail = self.tail
+                new_tail = new_node
+                current_tail.next = new_tail
+                self.tail = new_tail
+            else:
+                temp_node = self.head
+                index = 0
+                while index < location - 1:
+                    temp_node = temp_node.next
+                    index += 1
+                old_next = temp_node.next
+                new_next = new_node
+                temp_node.next = new_next
+                new_next.next = old_next
+
+
+                
+        
         
 class Node():
     def __init__(self, value):
@@ -20,6 +51,24 @@ class Node():
         self.next = None
         
 list1 = LinkedList()
-node1 = Node(5)
 
-print(list1, node1)
+list1.insert_into_list(5, 0)
+list1.insert_into_list(20, 0)
+list1.insert_into_list(99, 1)
+list1.insert_into_list(555, 1)
+
+print([node.value for node in list1])
+
+list2 = LinkedList()
+list2.insert_into_list(1, 1)
+list2.insert_into_list(2, 1)
+list2.insert_into_list(3, 1)
+list2.insert_into_list(4, 1)
+list2.insert_into_list(0, 0)
+list2.insert_into_list(0, 3) # inserted after the third element
+
+print([node.value for node in list2])
+
+
+
+
