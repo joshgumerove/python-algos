56de613eda23fb8b9efcc031aae650b369f63c1b
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-11 20:19:51 -0400

    create method to print stack

--- stacks/intro.py
+++ stacks/intro.py
@@ -1,13 +1,26 @@
 class Stack():
     def __init__(self):
-        self.stack = []
+        self.list = []
 
     def push(self, value):
-        self.stack.append(value)
-        return self.stack
+        self.list.append(value)
+
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
 
 
 first_stack = Stack()
 first_stack.push(10)
 first_stack.push(15)
-print(first_stack.push(20))
+print(first_stack)
+
+# should use Stack data structure when we utilize the latest data first
+# example: back button in the browser
+# peek(): returns the top element from the stack w/o removing it
+# isEmpty(): checks if there are any elements in the stack
+# isFull(): check if stack is full - note some languages have limits on list/array size
+# delete(): delete entire stack
+# implementing with a List is easy but may run into problems when stack grows
+# implementing with a LinkedList has better performance
