c4743fd51d05f96b343e0c2819e89e0f2574cc3e
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-23 04:55:01 -0400

    is_full method

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -1,6 +1,26 @@
 class Queue():
     def __init__(self, maxSize):
         self.items = maxSize * [None]
+        self.maxSize = maxSize
+        self.start = -1
+        self.top = -1
+
+    def __str__(self):
+        values = [str(x) for x in self.items]
+        return " ".join(values)
+
+    def is_full(self):
+        if (self.top + 1) == self.start:
+            return True
+        elif (self.start == 0) and (self.top + 1 == self.maxSize):
+            return True
+        else:
+            return False
+
+
+first_queue = Queue(5)
+print(first_queue)
+print(first_queue.is_full())
 
 # uses fixed capacity
 # note how elements are ignored when enqueueing and dequeueing
