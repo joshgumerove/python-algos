4016cae3ebce2f8372dad3c242b442e4f1189009
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-26 14:11:44 -0400

    refactor Queue to separate class

--- queues/linked_list.py
+++ queues/linked_list.py
@@ -18,8 +18,13 @@ class LinkedList():
             yield current_node
             current_node = current_node.next
 
+
+class Queue():
+    def __init__(self):
+        self.linked_list = LinkedList()
+
     def is_empty(self):
-        if self.head is None:
+        if self.linked_list.head is None:
             return True
         else:
             return False
@@ -27,37 +32,33 @@ class LinkedList():
     def enqueue(self, value):
         new_node = Node(value)
         if self.is_empty():
-            self.head = new_node
-            self.tail = new_node
+            self.linked_list.head = new_node
+            self.linked_list.tail = new_node
         else:
-            self.tail.next = new_node
-            self.tail = self.tail.next
+            self.linked_list.tail.next = new_node
+            self.linked_list.tail = self.linked_list.tail.next
 
     def dequeue(self):
         if self.is_empty():
             return "no values to remove"
-        removed_value = self.head.value
-        self.head = self.head.next
+        removed_value = self.linked_list.head.value
+        self.linked_list.head = self.linked_list.head.next
         return removed_value
 
     def peek(self):
-        return self.head.value
+        return self.linked_list.head.value
 
     def delete(self):
-        self.head = None
-        self.tail = None
+        self.linked_list.head = None
+        self.linked_list.tail = None
 
 
-queue = LinkedList()
+queue = Queue()
 queue.enqueue(10)
 queue.enqueue(15)
 queue.enqueue(20)
 
-print(queue.head.value)
-print(queue.tail.value)
-
 print(queue.dequeue())
-print(queue.head.value)
 print(queue.peek())
 print(queue.is_empty())
 
@@ -65,4 +66,4 @@ print(queue.is_empty())
 # print(queue.dequeue())
 # print(queue.dequeue())
 # print(queue.dequeue())
-print([node.value for node in queue])
+print([node.value for node in queue.linked_list])
