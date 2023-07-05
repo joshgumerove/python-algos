6c222596be52fa6f8356f6cfef89982644b3b6ab
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-07-05 14:33:29 -0400

    notes on tree data structure

---
+++ tree/index.py
@@ -0,0 +1,5 @@
+# used to represent data in heirarchical form
+# like a reverse tree in real life
+# more specialized as go down the tree
+# every node has two components (data and reference to its subcategory)
+# tree is a nonlinear data structure
