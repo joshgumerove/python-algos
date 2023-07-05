d4761f1e527c5edaec96918176c728947d70e3a7
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-07-05 16:09:51 -0400

    create basic tree

--- tree/index.py
+++ tree/index.py
@@ -1,3 +1,45 @@
+class TreeNode():
+    def __init__(self, data, children=[]):
+        self.data = data
+        self.children = children
+
+    def __str__(self, level=0):
+        ret = " " * level + str(self.data) + "\n"
+        for child in self.children:
+            ret += child.__str__(level + 1)
+        return ret
+
+    def addChild(self, TreeNode):
+        self.children.append(TreeNode)
+
+
+# create root first
+tree = TreeNode('Drinks', [])
+
+print(tree)
+
+cold = TreeNode('Cold', [])
+hot = TreeNode('Hot', [])
+
+tree.addChild(cold)
+tree.addChild(hot)
+
+print(tree)
+print("*********")
+
+tea = TreeNode("Tea", [])
+coffee = TreeNode("Coffee", [])
+cola = TreeNode("Cola", [])
+fanta = TreeNode("Fanta", [])
+
+hot.addChild(tea)
+hot.addChild(coffee)
+cold.addChild(cola)
+cold.addChild(fanta)
+
+print(tree)
+
+
 # used to represent data in heirarchical form
 # like a reverse tree in real life
 # more specialized as go down the tree
