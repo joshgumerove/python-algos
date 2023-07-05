720c2247091cd972b11011b4fa52b1ddbb692c81
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-07-05 17:02:05 -0400

    create binary tree

---
+++ tree/binary_tree.py
@@ -0,0 +1,11 @@
+class TreeNode():
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+
+# each node has at most two children
+# different kinds of binary trees
+
+new_bt = TreeNode('Drinks')
