433e6b9f9400b691c76901d0fa1299bbcdb274db
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-07-09 12:59:38 -0400

    level order traversal

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -56,3 +56,90 @@ post_order_traversal(new_bt)
 # each node has at most two children
 # different kinds of binary trees
 # note the different ways to traverse a binary tree
+
+
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        current_node = self.head
+        while current_node:
+            yield current_node
+            current_node = current_node.next
+
+
+class Queue():
+    def __init__(self):
+        self.linked_list = LinkedList()
+
+    def __str__(self):
+        values = [str(node) for node in self.linked_list]
+        return " ".join(values)
+
+    def is_empty(self):
+        if self.linked_list.head is None:
+            return True
+        else:
+            return False
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.linked_list.head = new_node
+            self.linked_list.tail = new_node
+        else:
+            self.linked_list.tail.next = new_node
+            self.linked_list.tail = self.linked_list.tail.next
+
+    def dequeue(self):
+        if self.is_empty():
+            return "no values to remove"
+
+        temporary_val = self.linked_list.head
+
+        if self.linked_list.head == self.linked_list.tail:
+            self.linked_list.head = None
+            self.linked_list.tail = None
+            return temporary_val
+
+        else:
+            self.linked_list.head = self.linked_list.head.next
+            return temporary_val
+
+    def peek(self):
+        if self.linked_list.head is not None:
+            return self.linked_list.head.value
+        else:
+            return "no elements in the queue"
+
+    def delete(self):
+        self.linked_list.head = None
+        self.linked_list.tail = None
+
+
+def level_order_traversal(root_node):  # go level by level
+    if not root_node:
+        return
+    custom_queue = Queue()
+    custom_queue.enqueue(root_node)
+    while not custom_queue.is_empty():
+        root = custom_queue.dequeue()
+        print(root.value.data)
+        if root.value.left_child is not None:
+            custom_queue.enqueue(root.value.left_child)
+        if root.value.right_child is not None:
+            custom_queue.enqueue(root.value.right_child)
+
+
+level_order_traversal(new_bt)
