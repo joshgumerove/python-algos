36b2606e6d2eefb14cd1645132324a87c44766d7
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-09-20 11:30:45 -0400

    create binary search tree

---
+++ binary-search-tree/index.py
@@ -0,0 +1,9 @@
+class BSTNode():
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+
+new_BST = BSTNode(None)
+print(new_BST)
