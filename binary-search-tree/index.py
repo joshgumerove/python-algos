61848b573cd8daab98d136f29d0cf19a06b0ae06
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-09-20 15:10:18 -0400

    insert node in bst

--- binary-search-tree/index.py
+++ binary-search-tree/index.py
@@ -5,5 +5,25 @@ class BSTNode():
         self.right_child = None
 
 
+def insert_node(rootNode, value):
+    if rootNode.data is None:
+        rootNode.data = value
+    elif value <= rootNode.data:
+        if rootNode.left_child is None:
+            rootNode.left_child = BSTNode(value)
+        else:
+            insert_node(rootNode.left_child, value)
+    else:
+        if rootNode.right_child is None:
+            rootNode.right_child = BSTNode(value)
+        else:
+            insert_node(rootNode.right_child, value)
+    return "The node has been successfully inserted"
+
+
 new_BST = BSTNode(None)
-print(new_BST)
+
+insert_node(new_BST, 10)
+insert_node(new_BST, 7)
+print(new_BST.data)
+print(new_BST.left_child.data)
