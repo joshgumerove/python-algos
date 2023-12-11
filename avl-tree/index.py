d4923986b875ff6598121701bbcedfb9fc813f90
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-12-10 22:47:47 -0500

    add traversal functions

--- avl-tree/index.py
+++ avl-tree/index.py
@@ -6,5 +6,29 @@ class AVLNode:
         self.height = 1  # need height property to determine if AVL is balanced
 
 
+def pre_order_traversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    pre_order_traversal(rootNode.left_child)
+    pre_order_traversal(rootNode.right_child)
+
+
+def in_order_traversal(rootNode):
+    if not rootNode:
+        return
+    in_order_traversal(rootNode.left_child)
+    print(rootNode.data)
+    in_order_traversal(rootNode.right_child)
+
+
+def post_order_traversal(rootNode):
+    if not rootNode:
+        return
+    post_order_traversal(rootNode.left_child)
+    post_order_traversal(rootNode.right_child)
+    print(rootNode.data)
+
+
 new_AVL = AVLNode(10)
 print(new_AVL.data)
