79949f6b597c26baf18293ea97e72da095b52c3f
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-12-10 22:44:30 -0500

    add height property to avl tree

--- avl-tree/index.py
+++ avl-tree/index.py
@@ -3,6 +3,7 @@ class AVLNode:
         self.data = data
         self.left_child = None
         self.right_child = None
+        self.height = 1  # need height property to determine if AVL is balanced
 
 
 new_AVL = AVLNode()
