a05f0b21ea49d463a727d578feaf04b14acb71d4
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-01-02 14:45:18 -0500

    level order traversal

--- binary-heap/index.py
+++ binary-heap/index.py
@@ -19,8 +19,19 @@ def size_of_heap(rootNode):
         return rootNode.heap_size
 
 
+def level_order_traversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        for i in range(1, rootNode.heap_size + 1):
+            print(rootNode.custom_list[i])
+
+
 new_heap = Heap(5)
 
 print(new_heap.custom_list)
 print(peek_of_heap(new_heap))
 print(size_of_heap(new_heap))
+
+print("**traversal**")
+print(level_order_traversal(new_heap))
