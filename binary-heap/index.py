def3b61aa3c9f78bf1f4de635548f8577dabeea5
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-01-02 11:43:02 -0500

    size of heap

--- binary-heap/index.py
+++ binary-heap/index.py
@@ -1,7 +1,7 @@
 class Heap():
     def __init__(self, size):
         self.custom_list = (size + 1) * [None]
-        self.heapSize = 0
+        self.heap_size = 0
         self.max_size = size + 1
 
 
@@ -12,7 +12,15 @@ def peek_of_heap(rootNode):
         return rootNode.custom_list[1]
 
 
+def size_of_heap(rootNode):
+    if not rootNode:
+        return
+    else:
+        return rootNode.heap_size
+
+
 new_heap = Heap(5)
 
 print(new_heap.custom_list)
 print(peek_of_heap(new_heap))
+print(size_of_heap(new_heap))
