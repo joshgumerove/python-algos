a64697155a62bc323e40dcdfbcc6179cc2a1c251
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-01-25 09:10:55 -0500

    insert string

--- trie/index.py
+++ trie/index.py
@@ -8,6 +8,19 @@ class Trie():
     def __init__(self):
         self.root = TrieNode()
 
+    def insert_string(self, word):
+        current = self.root
+        for i in word:
+            ch = i
+            node = current.children.get(ch)
+            if node == None:
+                node = TrieNode()
+                current.children.update({ch: node})
+            current = node
+        current.end_of_string = True
+        print("Successfully inserted")
+
 
 new_trie = Trie()
-print(new_trie.root)
+new_trie.insert_string("App")
+new_trie.insert_string("Appl")
