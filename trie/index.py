c8d3b6564d770a095458766927e9bf50200cae44
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-01-28 21:23:03 -0500

    delete string

--- trie/index.py
+++ trie/index.py
@@ -34,6 +34,34 @@ class Trie():
             return False
 
 
+def delete_string(root, word, index):
+    ch = word[index]
+    current_node = root.children.get(ch)
+    can_be_deleted = False
+
+    if len(current_node.children) > 1:
+        delete_string(current_node, word, index + 1)
+        return False
+    if index == len(word) - 1:
+        if len(current_node.children) >= 1:
+            current_node.end_of_string = False
+            return False
+        else:
+            root.children.pop(ch)
+            return True
+    if current_node.end_of_string == True:
+        delete_string(current_node, word, index + 1)
+        return False
+
+    can_be_deleted = delete_string(current_node, word, index + 1)
+
+    if can_be_deleted == True:
+        root.children.pop(ch)
+        return True
+    else:
+        return False
+
+
 new_trie = Trie()
 new_trie.insert_string("App")
 new_trie.insert_string("Appl")
@@ -42,3 +70,7 @@ print("***search_for_string***")
 print(new_trie.search_for_string("App"))
 print(new_trie.search_for_string("Ap"))
 print(new_trie.search_for_string("DCD"))
+
+print("***delete_string***")
+delete_string(new_trie.root, "App", 0)
+print(new_trie.search_for_string("App"))
