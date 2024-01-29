e25b6c512dd9ce1be0129879f1a9599c9a4e8e16
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-01-28 20:46:24 -0500

    insert string in trie

--- trie/index.py
+++ trie/index.py
@@ -20,7 +20,25 @@ class Trie():
         current.end_of_string = True
         print("Successfully inserted")
 
+    def search_for_string(self, word):
+        current_node = self.root
+
+        for i in word:
+            node = current_node.children.get(i)
+            if node == None:
+                return False
+            current_node = node
+        if current_node.end_of_string == True:
+            return True
+        else:
+            return False
+
 
 new_trie = Trie()
 new_trie.insert_string("App")
 new_trie.insert_string("Appl")
+
+print("***search_for_string***")
+print(new_trie.search_for_string("App"))
+print(new_trie.search_for_string("Ap"))
+print(new_trie.search_for_string("DCD"))
