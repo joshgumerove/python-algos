f74ed61d7c1fa9baccd8ef7b772d4d33958fc3b7
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-02-09 16:46:17 -0500

    remove note

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,6 +1,6 @@
 def bubble_sort(customList):
     for i in range(len(customList) - 1):
-        for j in range(len(customList) - i - 1):  # decrease number of loops as
+        for j in range(len(customList) - i - 1):  # decrease number of loops
             if customList[j] > customList[j + 1]:
                 customList[j], customList[j +
                                           1] = customList[j + 1], customList[j]
