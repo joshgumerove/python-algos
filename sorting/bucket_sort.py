9d512cdcd16f795ea91d8a24cff516143626ace9
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-03-01 15:19:10 -0500

    bucket sort

--- sorting/bucket_sort.py
+++ sorting/bucket_sort.py
@@ -15,7 +15,22 @@ def insertion_sort(customList):
 def bucket_sort(customList):
     number_of_buckets = round(math.sqrt(len(customList)))
     max_value = max(customList)
-    print(max_value)
+    arr = []
+
+    for i in range(number_of_buckets):
+        arr.append([])
+    for j in customList:
+        index_b = math.ceil(j * number_of_buckets / max_value)
+        arr[index_b - 1].append(j)
+
+    for i in range(number_of_buckets):
+        arr[i] = insertion_sort(arr[i])
+    k = 0
+    for i in range(number_of_buckets):
+        for j in range(len(arr[i])):
+            print(customList)
+            k += 1
+    return customList
 
 
 list1 = [2, 1, 7, 6, 5, 3, 4, 8]
