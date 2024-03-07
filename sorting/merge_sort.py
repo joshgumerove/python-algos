04f2006c469932496262c56ad55f314e9b64483a
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-03-07 17:03:35 -0500

    refactor

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -6,17 +6,16 @@ def merge(customList, l, m, r):
     R = [0] * (n2)
 
     for i in range(0, n1):
-        L[i] = customList[l + i]
+        L[i] = customList[l+i]
 
     for j in range(0, n2):
-        R[j] = customList[m + 1 + j]
+        R[j] = customList[m+1+j]
 
     i = 0
     j = 0
     k = l
-
     while i < n1 and j < n2:
-        if L[i] < R[j]:
+        if L[i] <= R[j]:
             customList[k] = L[i]
             i += 1
         else:
@@ -27,7 +26,21 @@ def merge(customList, l, m, r):
         customList[k] = L[i]
         i += 1
         k += 1
+
     while j < n2:
         customList[k] = R[j]
-        i += 1
+        j += 1
         k += 1
+
+
+def mergeSort(customList, l, r):
+    if l < r:
+        m = (l+(r-1))//2
+        mergeSort(customList, l, m)
+        mergeSort(customList, m+1, r)
+        merge(customList, l, m, r)
+    return customList
+
+
+clist = [2, 1, 7, 6, 5, 3, 4, 9, 8]
+print(mergeSort(clist, 0, 8))
