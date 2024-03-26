cd0c95c2302d451c1f42791228324f2dd877417e
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-03-26 11:33:56 -0400

    add edge method

--- graphs/index.py
+++ graphs/index.py
@@ -3,3 +3,22 @@ class Graph():
         if gdict is None:
             gdict = {}
         self.gdict = gdict
+
+    def add_edge(self, vertex, edge):
+        self.gdict[vertex].append(edge)
+
+
+custom_dict = {
+    "a": ["b", "c"],
+    "b": ["a", "d", "e"],
+    "c": ["a", "e"],
+    "d": ["b", "e", "f"],
+    "e": ["b", "d", "f"],
+    "f": ["d", "e"]
+}
+
+graph = Graph(custom_dict)
+print(graph.gdict)
+
+graph.add_edge("e", "c")
+print(graph.gdict)
