b8832ae4f9f182d8e38622b940971e651f68e532
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-04-12 11:20:15 -0400

    create classes for edge and node

---
+++ graphs/sssp.py
@@ -0,0 +1,17 @@
+class Edge():
+    def __init__(self, weight, start_vertex, target_vertex):
+        self.weight = weight
+        self.start_vertex = start_vertex
+        self.target_vertex = target_vertex
+
+
+class Node():
+    def __init__(self, name):
+        self.name = name
+        self.visited = False
+        self.predecessor = None  # previous node we came from
+        self.neighbors = []
+        self.min_distance = float('inf')
+
+    def __lt__(self, other_node):
+        return self.min_distance < other_node.min_distance
