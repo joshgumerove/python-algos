10b448e029e708c817fe1f857ee577b3b814f3f4
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-04-28 14:55:12 -0400

    test algorithm

--- graphs/bellman_ford.py
+++ graphs/bellman_ford.py
@@ -23,9 +23,29 @@ class Graph():
                 if dist[s] != float('inf') and dist[s] + w < dist[d]:
                     dist[d] = dist[s] + w
 
-        for s, d, we in self.graph:
+        for s, d, w in self.graph:
             if dist[s] != float('Inf') and dist[s] + w < dist[d]:
                 print("The graph contains a negative cycle")
                 return
 
         self.print_solution(dist)
+
+
+g = Graph(5)
+
+g.add_node("A")
+g.add_node("B")
+g.add_node("C")
+g.add_node("D")
+g.add_node("E")
+
+g.add_edge("A", "C", 6)
+g.add_edge("A", "D", 6)
+g.add_edge("B", "A", 3)
+g.add_edge("C", "D", 1)
+g.add_edge("D", "C", 2)
+g.add_edge("D", "B", 1)
+g.add_edge("E", "B", 4)
+g.add_edge("E", "D", 2)
+
+g.bellman_ford("E")
