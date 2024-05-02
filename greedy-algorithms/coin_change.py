d6778436aada233fafa684a58780425e319e9c7e
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-01 22:37:26 -0400

    coin change solution

--- greedy-algorithms/coin_change.py
+++ greedy-algorithms/coin_change.py
@@ -2,8 +2,20 @@ def coin_change(totalNumber, coins):
     N = totalNumber
     coins.sort()
     index = len(coins) - 1
+    count = 0
+    while True:
+        coin_value = coins[index]
+        if N >= coin_value:
+            count += 1
+            print(coin_value)
+            N -= coin_value
+        if N < coin_value:
+            index -= 1
 
-    print(index)
+        if N == 0:
+            return print(f"total coins needed {count}")
 
 
-coin_change(30, [10, 9, 5, 6, 11])
+coins = [1, 2, 5, 20, 50, 100]
+
+coin_change(201, coins)
