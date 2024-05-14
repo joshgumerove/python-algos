50187f696a5eb1d874c96b67cc89f39d3c0e14a4
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-14 19:12:44 -0400

    fibonacci with tabulation

--- dyanmic-programming/fib.py
+++ dyanmic-programming/fib.py
@@ -9,3 +9,13 @@ def fib_memo(n, memo):
 
 cache = {}
 print(fib_memo(11, cache))
+
+def fib_tab(n):
+    tb = [0, 1]
+    
+    for i in range(2, n + 1):
+        tb.append(tb[i -1] + tb[i - 2])
+    return tb[n -1]
+
+print(fib_tab(10))
+        
