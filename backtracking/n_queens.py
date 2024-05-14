5a38b5b24672e6c499ea8e514df91315ec0ac1f4
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-13 23:08:15 -0400

    safe place method

--- backtracking/n_queens.py
+++ backtracking/n_queens.py
@@ -1,7 +1,42 @@
 class NQueens():
     def __init__(self, n):
+        self.n = n
         self.chess_table = [[0 for i in range(n)] for j in range(n)]
+        
+    def print_queens(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.chess_table[i][j] == 1:
+                    print(" Q ", end='')
+                else:
+                    print(" - ", end='')
+            print("\n")
+            
+    def is_safe_place(self, row_index, col_index):
+        for i in range(self.n): # checks horizontally
+            if self.chess_table[row_index][i]:
+                return False
+        j = col_index
+        for i in range(row_index, -1, -1): # top left to bottom right
+            if i < 0:
+                break
+            if self.chess_table[i][j] == 1:
+                return False
+            j = j - 1
+            
+        j = col_index
+        for i in range(row_index, self.n): # top right to bottom left
+            if i < 0:
+                break
+            if self.chess_table[i][j] == 1:
+                return False
+            j = j - 1
+        
+        return True
+            
+    
         
 
 queens = NQueens(4)
-print(queens.chess_table)
+queens.print_queens()
+
